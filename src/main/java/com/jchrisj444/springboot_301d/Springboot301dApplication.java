482e0bbfded0ed55972351676a448cae2b2ef252
GBTC440012UR <GBTC440012UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-03-25 20:27:44 -0400

    Initial commit

---
+++ src/main/java/com/jchrisj444/springboot_301d/Springboot301dApplication.java
@@ -0,0 +1,13 @@
+package com.jchrisj444.springboot_301d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot301dApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot301dApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jchrisj444/springboot_301d/Springboot301dApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jchrisj444.springboot_301d;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot301dApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
